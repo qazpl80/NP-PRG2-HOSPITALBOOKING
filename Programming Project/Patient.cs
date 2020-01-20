d5b3ccf60ac77204d673a189cfc5a3aab1dc2a50
Yuan Ming Tan <s10198319@connect.np.edu.sg>
2020-01-20 11:12:17 +0800

    Modified Patient.cs

--- Programming Project/Patient.cs
+++ Programming Project/Patient.cs
@@ -1,10 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Patient
+namespace Programming_Project
 {
-	public Patient()
-	{
-        public string id { get; set; }
-        public int MyProperty { get; set; }
+    class Patient
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public char Gender { get; set; }
+        public string CitizenStatus { get; set; }
+        public string Status { get; set; }
+        public stay Stay { get; set; }
+
+        public Patient(string id, string na, int ag, char gd, string ctz, string sts)
+        {
+            Id = id;
+            Name = na;
+            Age = ag;
+            Gender = gd;
+            CitizenStatus = ctz;
+            Status = sts;
+        }
+        public double CalculateCharges() { }
+        public override string ToString()
+        {
+            return "ID: " + Id
+                + "\tName: " + Name
+                + "\tAge: " +  Age
+                + "\tGender" + Gender
+                + "\tCitizen Status: " + CitizenStatus
+                + "\tStatus: " + Status;
+        }
     }
 }
