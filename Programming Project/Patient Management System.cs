60b42d9372719a4141011f89e451736cbaf483cd
Yuan Ming Tan <s10198319@connect.np.edu.sg>
2020-01-20 12:39:48 +0800

    Added blocked comment at the top stating the student number(s), name(s) and group

--- Programming Project/Patient Management System.cs
+++ Programming Project/Patient Management System.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//============================================================
+// Student Number	: S10198319, S
+// Student Name	: Tan Yuan Ming, Gladys
+// Module  Group	: P08 //============================================================
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
