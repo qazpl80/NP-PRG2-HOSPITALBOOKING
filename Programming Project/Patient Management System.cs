4fa8200e6a4653fcee6c4db849346ecf106d8260
Ling Hui Gladys Chua <s10196678@connect.np.edu.sg>
2020-01-23 10:36:07 +0800

    Added displayMenu() to main program

--- Programming Project/Patient Management System.cs
+++ Programming Project/Patient Management System.cs
@@ -16,7 +16,26 @@ namespace Programming_Project
     {
         static void Main(string[] args)
         {
-            //Testing one
+            
+
+        }
+        static void DisplayMenu()
+        {
+            Console.WriteLine("MENU");
+            Console.WriteLine("====");
+            Console.WriteLine("1. View all patients");
+            Console.WriteLine("2. View all beds");
+            Console.WriteLine("3. Register patient");
+            Console.WriteLine("4. Add new bed");
+            Console.WriteLine("5. Register a hospital stay");
+            Console.WriteLine("6. Retrieve patient details");
+            Console.WriteLine("7. Add medical record entry");
+            Console.WriteLine("8. View medical records");
+            Console.WriteLine("9. Transfer patient to another bed");
+            Console.WriteLine("10. Discharge and payment");
+            Console.WriteLine("11. Display currencies exchange rate");
+            Console.WriteLine("12. Display PM 2.5 information");
+            Console.WriteLine("0. Exit");
         }
     }
 }
